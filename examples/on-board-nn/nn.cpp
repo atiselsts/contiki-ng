00197fdd4f0dd56016a39005dec97ef6fc6bd31b
Atis Elsts <atis.elsts@gmail.com>
2020-11-12 11:38:50 +0200

    start filling NN example code

--- examples/on-board-nn/nn.cpp
+++ examples/on-board-nn/nn.cpp
@@ -9,6 +9,7 @@ extern "C" {
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
+#include "tensorflow/lite/micro/all_ops_resolver.h"
 
 
 tflite::ErrorReporter* error_reporter = nullptr;
@@ -20,8 +21,8 @@ int input_length;
 // Create an area of memory to use for input, output, and intermediate arrays.
 // The size of this will depend on the model you're using, and may need to be
 // determined by experimentation.
-constexpr int kTensorArenaSize = 60 * 1024;
-uint8_t tensor_arena[kTensorArenaSize];
+constexpr int tensor_arena_size = 60 * 1024;
+uint8_t tensor_arena[tensor_arena_size];
 
 void operator delete(void*) {
   /* no-op */
@@ -29,6 +30,20 @@ void operator delete(void*) {
 
 int nn_classify_single(const float features[])
 {
+  model_input->data.f[0] = 0;
+
+  // Run inference, and report any error.
+  TfLiteStatus invoke_status = interpreter->Invoke();
+  if (invoke_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed on index\n");
+    return 0;
+  }
+
+  float *f = interpreter->output(0)->data.f;
+
+  TF_LITE_REPORT_ERROR(error_reporter, "Output 0: %f\n",
+                       f[0]);
+
   return 0;
 }
 
@@ -49,6 +64,31 @@ extern "C" void nn_setup(void)
                          model->version(), TFLITE_SCHEMA_VERSION);
     return;
   }
+
+  tflite::AllOpsResolver resolver;
+
+  static tflite::MicroInterpreter static_interpreter(model, resolver, tensor_arena,
+                                                     tensor_arena_size, error_reporter);
+
+  interpreter = &static_interpreter;
+
+  interpreter->AllocateTensors();
+
+  model_input = interpreter->input(0);
+
+  TF_LITE_REPORT_ERROR(error_reporter,
+      "dims->size=%u\n", model_input->dims);
+
+  if (/*(model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
+      (model_input->dims->data[1] != 128) ||
+      (model_input->dims->data[2] != kChannelNumber) || */
+      (model_input->type != kTfLiteFloat32)) {
+    TF_LITE_REPORT_ERROR(error_reporter,
+                         "Bad input tensor parameters in model");
+    return;
+  }
+
+  input_length = model_input->bytes / sizeof(float);
 
   TF_LITE_REPORT_ERROR(error_reporter,
       "Hello world from TF!\n");
